60e1abaaa0c779028ff71e9b3b6a9a26d87d7de0
StjepanMat <stjepan.mateljan@gmail.com>
2024-01-11 11:37:30 +0100

    Models/ProductImage configured

--- Webshop/Models/ProductImage.cs
+++ Webshop/Models/ProductImage.cs
@@ -1,6 +1,26 @@
-﻿namespace Webshop.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Webshop.Models
 {
     public class ProductImage
     {
+        [Key]
+        public int Id { get; set; }
+
+        public int ProductId { get; set; }
+
+        public bool IsMainImage { get; set; }
+
+        [StringLength(200)]
+        public string Title { get; set; }
+
+        [StringLength(500)]
+        public string FileName { get; set; }
+
+        [NotMapped]
+        public string ProductTitle { get; set; }
+
+
     }
 }
