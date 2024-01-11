7ea9e21cdc5c256508409c0e01d3704690b1a9c8
StjepanMat <stjepan.mateljan@gmail.com>
2024-01-11 09:58:45 +0100

    Models/Category created and configured; Models/ProductCategory created

---
+++ Webshop/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.CodeAnalysis;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace Webshop.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        [StringLength(200)]
+        public string Title { get; set; }
+
+        [ForeignKey("CategoryID")]
+        public virtual ICollection<ProductCategory> ProductCategories { get; set; }
+
+    }
+}
