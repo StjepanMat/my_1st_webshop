ff5bc9c74df7585d2f4f919782f0722b9ea30df4
StjepanMat <stjepan.mateljan@gmail.com>
2024-01-11 11:06:08 +0100

    Models/Product created, configured; /OrderItem, ProductImage created

---
+++ Webshop/Models/Product.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Webshop.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+        
+        [Required]
+        public int Quantity { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(9,2)")]
+        public decimal Price { get; set; }
+
+        [ForeignKey("ProductId")]
+        public virtual ICollection<ProductCategory> ProductCategories { get; set; }
+
+        [ForeignKey("ProductId")]
+        public virtual ICollection<OrderItem> OrderItems { get; set; }
+
+        [ForeignKey("ProductId")]
+        public virtual ICollection<ProductImage> ProductImages { get; set; }
+    }
+}
