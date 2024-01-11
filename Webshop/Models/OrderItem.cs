7b890723d8425c71299eff633481d6b589e11c50
StjepanMat <stjepan.mateljan@gmail.com>
2024-01-11 11:20:10 +0100

    Models/OrderItem configured

--- Webshop/Models/OrderItem.cs
+++ Webshop/Models/OrderItem.cs
@@ -1,6 +1,24 @@
-﻿namespace Webshop.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Webshop.Models
 {
     public class OrderItem
     {
+        [Key]
+        public int Id { get; set; }
+        public int OrderId { get; set; }
+        public int ProductId { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(9,2)")]
+        public decimal Price { get; set; }
+
+        [NotMapped]
+        public string ProductTitle { get; set; }
+
     }
 }
